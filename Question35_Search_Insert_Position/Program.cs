1e3f5a9396018456ce60014356c3d4931f021c39
tunahan <tunahancelebi@derinbilgi.com.tr>
2024-02-22 10:37:12 +0300

    Question35 Search Insert Position

---
+++ Question35_Search_Insert_Position/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var input = new[] { 1, 3, 6, 6 };
+var result = Solution.SearchInsert(input, 7);
+Console.WriteLine(result);
+
+stopwatch.Stop();
+var elapsedTime = stopwatch.Elapsed;
+var elapsedSeconds = elapsedTime.TotalSeconds;
+
+Console.WriteLine(elapsedTime);
+Console.WriteLine(elapsedSeconds);
+
+
+public static class Solution
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int i = 0;
+        for (; i < nums.Length; i++)
+        {
+            if (target <= nums[i])
+                return i;
+        }
+
+        return i;
+    }
+}
