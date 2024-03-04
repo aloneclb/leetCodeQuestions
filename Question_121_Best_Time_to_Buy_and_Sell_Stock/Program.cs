a0fbf3e6093408a573753e2e39172719cd283f32
tunahan <tunahancelebi@derinbilgi.com.tr>
2024-03-04 17:42:58 +0300

    Question_136_Single_Number.csproj, Question_121_Best_Time_to_Buy_and_Sell_Stock.csproj

---
+++ Question_121_Best_Time_to_Buy_and_Sell_Stock/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+
+int[] prices = [7, 1, 5, 3, 6, 4];
+
+var result = Solution.MaxProfit(prices);
+
+Console.WriteLine(result);
+
+stopwatch.Stop();
+var elapsedTime = stopwatch.Elapsed;
+var elapsedSeconds = elapsedTime.TotalSeconds;
+
+Console.WriteLine(elapsedTime);
+Console.WriteLine(elapsedSeconds);
+
+public static class Solution
+{
+    public static int MaxProfit(int[]? prices)
+    {
+        if (prices == null || prices.Length < 2)
+            return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            int currentPrice = prices[i];
+            int currentProfit = currentPrice - minPrice;
+            maxProfit = Math.Max(maxProfit, currentProfit);
+            minPrice = Math.Min(minPrice, currentPrice);
+        }
+
+        return maxProfit;
+    }
+}
---
+++ Question_136_Single_Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+Console.WriteLine(3 ^ 5);
+
+int[] prices = [2, 1, 2, 3, 3, 4, 4, 55, 1];
+
+var result = Solution.SingleNumber(prices);
+
+Console.WriteLine(result);
+
+stopwatch.Stop();
+var elapsedTime = stopwatch.Elapsed;
+var elapsedSeconds = elapsedTime.TotalSeconds;
+
+Console.WriteLine(elapsedTime);
+Console.WriteLine(elapsedSeconds);
+
+
+public static class Solution
+{
+    // Hashsetlerde kullanılabilirdi
+    
+    public static int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach (var num in nums)
+        {
+            result ^= num;
+        }
+
+        return result;
+    }
+    
+    // XOR 
+    // 0 XOR 0 = 0
+    // 0 XOR 1 = 1
+    // 1 XOR 0 = 1
+    // 1 XOR 1 = 0
+    
+    // 0 1 1   -> 3 
+    // 1 0 1   -> 5
+//XOR--------------------
+    // 1 1 0   -> 6
+}
