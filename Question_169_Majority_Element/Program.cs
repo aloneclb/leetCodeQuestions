b2d68667d5d0aa54db0b7eb38449991b0291e1df
tunahan <tunahancelebi@derinbilgi.com.tr>
2024-03-10 18:42:24 +0300

    Question_169_Majority_Element

---
+++ Question_169_Majority_Element/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+
+var result = Solution.MajorityElement([1, 2, 3, 4, 5, 5, 5, 5, 3, 3, 2, 2, 2, 1, 2]);
+
+Console.WriteLine(result);
+
+stopwatch.Stop();
+var elapsedTime = stopwatch.Elapsed;
+var elapsedSeconds = elapsedTime.TotalSeconds;
+
+Console.WriteLine(elapsedTime);
+Console.WriteLine(elapsedSeconds);
+
+
+public static class Solution
+{
+    public static int MajorityElement(int[] nums)
+    {
+        HashSet<int> myHashSet = new HashSet<int>(nums);
+        var temp = 0;
+        var number = 0;
+        foreach (int num in myHashSet)
+        {
+            var numberCount = nums.Count(x => x == num);
+            if (temp < numberCount)
+            {
+                temp = numberCount;
+                number = num;
+            }
+        }
+
+        return number;
+    }
+}
